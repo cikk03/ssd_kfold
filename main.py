1c8b83a8c931aa6f90bc9bbc07a9900ffc353bf2
cikk03 <tbvjdlsrb12@gmail.com>
2025-02-11 09:15:57 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import SSD       # SSD.py 파일
+import ensemble  # ensemble.py 파일
+
+def main():
+    st.sidebar.title("실행 옵션")
+    choice = st.sidebar.radio("실행할 기능 선택", ("SSD 분석", "Ensemble 실행"))
+    
+    if choice == "SSD 분석":
+        SSD.ssd_run()
+    elif choice == "Ensemble 실행":
+        ensemble.ensemble_run()
+
+if __name__ == "__main__":
+    main()
